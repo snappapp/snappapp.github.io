c00751c6a87bc644ba5e0de2633f6c03116f98c5
Mehdi Faraji <mahdifarajideveloper@gmail.com>
2020-06-06 22:07:34 +0330

    'commit'

---
+++ precache-manifest.a10732e672a6ef9dd192186a7f3aa8bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6130dad2a6ad2505d143a2a285083902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e824be32a920aa6d0ad5",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "05778bca9565ff8490c0",
+    "url": "/static/css/main.698586c9.chunk.css"
+  },
+  {
+    "revision": "e824be32a920aa6d0ad5",
+    "url": "/static/js/2.07784b6f.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.07784b6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05778bca9565ff8490c0",
+    "url": "/static/js/main.146fb9a1.chunk.js"
+  },
+  {
+    "revision": "d895f2ae8d1ce5378ab2",
+    "url": "/static/js/runtime-main.8368cf0f.js"
+  },
+  {
+    "revision": "ecc4730335e1a6ffc7590f3fad642271",
+    "url": "/static/media/logo.ecc47303.png"
+  }
+]);
